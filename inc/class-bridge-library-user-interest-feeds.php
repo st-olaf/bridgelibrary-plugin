6d62269e032dc21462a22dcde5374b3062054c15
Andrew Minion <andrew.minion@luminfire.com>
2021-02-26 16:19:52 -0600

    improve URL slugs

--- inc/class-bridge-library-user-interest-feeds.php
+++ inc/class-bridge-library-user-interest-feeds.php
@@ -103,11 +103,14 @@ class Bridge_Library_User_Interest_Feeds {
 					'read_post'           => 'read_user_interest_feed',
 				),
 				'admin_cols'          => array(
-					'feed_url'  => array(
+					'feed_url' => array(
 						'title'    => 'Feed URL',
 						'meta_key' => 'feed_url', // phpcs:ignore WordPress.DB.SlowDBQuery.slow_db_query_meta_key
 					),
 				),
+			),
+			array(
+				'slug' => 'user-interest-feeds',
 			)
 		);
 	}
