6be1587498cf021a6909fe8b9a092504446c4eb4
Andrew Minion <andrew.minion@luminfire.com>
2021-03-03 16:33:07 -0600

    use menu_order to order user interest feeds

--- inc/class-bridge-library-user-interest-feeds.php
+++ inc/class-bridge-library-user-interest-feeds.php
@@ -401,6 +401,8 @@ class Bridge_Library_User_Interest_Feeds {
 					array(
 						'post_type'      => $this->post_type,
 						'posts_per_page' => -1,
+						'orderby'        => 'menu_order',
+						'order'          => 'ASC',
 					)
 				);
 
