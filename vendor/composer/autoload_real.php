a12416c53bf22bebfff67037bce0d9f94fc06648
Andrew Minion <andrew.minion@luminfire.com>
2023-04-24 09:43:53 -0500

    update dependencies

--- vendor/composer/autoload_real.php
+++ vendor/composer/autoload_real.php
@@ -33,25 +33,18 @@ class ComposerAutoloaderInitb714f90ae24068d19124a6c7781f4fdb
 
         $loader->register(true);
 
-        $includeFiles = \Composer\Autoload\ComposerStaticInitb714f90ae24068d19124a6c7781f4fdb::$files;
-        foreach ($includeFiles as $fileIdentifier => $file) {
-            composerRequireb714f90ae24068d19124a6c7781f4fdb($fileIdentifier, $file);
+        $filesToLoad = \Composer\Autoload\ComposerStaticInitb714f90ae24068d19124a6c7781f4fdb::$files;
+        $requireFile = \Closure::bind(static function ($fileIdentifier, $file) {
+            if (empty($GLOBALS['__composer_autoload_files'][$fileIdentifier])) {
+                $GLOBALS['__composer_autoload_files'][$fileIdentifier] = true;
+
+                require $file;
+            }
+        }, null, null);
+        foreach ($filesToLoad as $fileIdentifier => $file) {
+            $requireFile($fileIdentifier, $file);
         }
 
         return $loader;
-    }
-}
-
-/**
- * @param string $fileIdentifier
- * @param string $file
- * @return void
- */
-function composerRequireb714f90ae24068d19124a6c7781f4fdb($fileIdentifier, $file)
-{
-    if (empty($GLOBALS['__composer_autoload_files'][$fileIdentifier])) {
-        $GLOBALS['__composer_autoload_files'][$fileIdentifier] = true;
-
-        require $file;
     }
 }
